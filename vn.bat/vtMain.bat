2019dc34e8e8c467b3fdc8ea2d6f1c1071fe994f
your name <you@example.com>
2017-04-24 13:41:49 +0800

    增加selenium研究代码

--- vn.bat/vtMain.bat
+++ vn.bat/vtMain.bat
@@ -1,5 +1,5 @@
 @echo off
 cd..
 cd vn.trader
-python vtMain.py
+python vtMain.py "1" "_simnow.json" "_dh_he.json" "_dh_feng.json"
 pause
--- vn.trader/vtMain.py
+++ vn.trader/vtMain.py
@@ -1,5 +1,12 @@
 # encoding: UTF-8
 
+'''
+History
+<id>            <author>        <description>
+2017042400      hetajen         根据bat脚本参数自动选择交易账户
+
+'''
+
 import sys
 import os
 import ctypes
@@ -10,12 +17,12 @@ from vtEngine import MainEngine
 from uiMainWindow import *
 
 # 文件路径名
-path = os.path.abspath(os.path.dirname(__file__))    
+path = os.path.abspath(os.path.dirname(__file__))
 ICON_FILENAME = 'vnpy.ico'
-ICON_FILENAME = os.path.join(path, ICON_FILENAME)  
+ICON_FILENAME = os.path.join(path, ICON_FILENAME)
 
 SETTING_FILENAME = 'VT_setting.json'
-SETTING_FILENAME = os.path.join(path, SETTING_FILENAME)  
+SETTING_FILENAME = os.path.join(path, SETTING_FILENAME)
 
 #----------------------------------------------------------------------
 def main():
@@ -23,26 +30,26 @@ def main():
     # 重载sys模块，设置默认字符串编码方式为utf8
     reload(sys)
     sys.setdefaultencoding('utf8')
-    
+
     # 设置Windows底部任务栏图标
     if 'Windows' in platform.uname() :
-        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('vn.trader')  
-    
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('vn.trader')
+
     # 初始化Qt应用对象
     app = QtGui.QApplication(sys.argv)
     app.setWindowIcon(QtGui.QIcon(ICON_FILENAME))
     app.setFont(BASIC_FONT)
-    
+
     # 设置Qt的皮肤
     try:
         f = file(SETTING_FILENAME)
-        setting = json.load(f)    
+        setting = json.load(f)
         if setting['darkStyle']:
             import qdarkstyle
             app.setStyleSheet(qdarkstyle.load_stylesheet(pyside=False))
     except:
         pass
-    
+
     # 初始化主引擎和主窗口对象
     mainEngine = MainEngine()
     mainWindow = MainWindow(mainEngine, mainEngine.eventEngine)
@@ -50,6 +57,25 @@ def main():
 
     # 在主线程中启动Qt事件循环
     sys.exit(app.exec_())
-    
+
+'''2017042400 Add by hetajen begin'''
+def choose_user(argv):
+    if len(argv) > 1:
+        path_src = os.path.join(os.path.abspath('.'), u'ctpGateway\\CTP_connect' + unicode(argv[int(argv[1]) + 1]))
+    else:
+        path_src = os.path.join(os.path.abspath('.'), u'ctpGateway\\CTP_connect_simnow.json')
+
+    path_dst = os.path.join(os.path.abspath('.'), u'ctpGateway\\CTP_connect.json')
+    try:
+        with open(unicode(path_src), 'r') as src:
+            with open(unicode(path_dst), "w") as dst:
+                dst.write(src.read())
+    except IOError:
+        print "func(%s)'s Error: %s" % (sys._getframe().f_code.co_name, IOError)
+'''2017042400 Add by hetajen end'''
+
 if __name__ == '__main__':
+    '''2017042400 Add by hetajen begin'''
+    choose_user(sys.argv)
+    '''2017042400 Add by hetajen end'''
     main()
